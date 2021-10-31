2fec01b78395fa413d5aa44fb36e8583c98e704f
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-31 22:44:53 +0300

    fast-company_project repetition of forms__controlled and uncontrolled components__3

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const handleChange = (event) => {
+        setEmail(event.target.value);
+        console.log(event.target.value);
+    };
+
     return (
         <form action="">
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleChange}
+                />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
