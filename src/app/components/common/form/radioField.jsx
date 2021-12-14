0446301bd75c5aaba63eb6dd5e483304b27a0fb3
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-12-14 23:58:18 +0300

    fast-company_project repetition of forms__radioField__22

---
+++ src/app/components/common/form/radioField.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const RadioField = ({ options, name, onChange, value }) => {
+    return <div></div>;
+};
+
+RadioField.propTypes = {
+    options: PropTypes.array,
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    value: PropTypes.string
+};
+
+export default RadioField;
