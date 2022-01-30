ecd0b45f1c231cdd84b856600f6a75181222edd1
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-01-31 01:51:26 +0300

    fast-company_project repetition of forms__removing_yup_library_for_validation__29

--- src/app/components/ui/loginForm.jsx
+++ src/app/components/ui/loginForm.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-// import { validator } from "../../utils/validator";
+import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
 import CheckBoxField from "../common/form/checkBoxField";
-import * as yup from "yup";
 
 const LoginForm = () => {
     const [data, setData] = useState({
@@ -19,45 +18,45 @@ const LoginForm = () => {
         console.log(target.name);
     };
 
-    const validateScheme = yup.object().shape({
-        password: yup
-            .string()
-            .required("Password isRequired")
-            .matches(/(?=.*[A-Z])/, "Password isRequired")
-            .matches(/(?=.*[0-9])/, "Password must contain at least one number")
-            .matches(
-                /(?=.*[!@#$%^&*])/,
-                "Password must contain one of the special characters !@#$%^&*"
-            )
-            .matches(
-                /(?=.{8,})/,
-                "Password must be at least 8 characters long"
-            ),
-        email: yup
-            .string()
-            .required("Email isRequired")
-            .email("Email entered incorrectly")
-    });
+    // const validateScheme = yup.object().shape({
+    //     password: yup
+    //         .string()
+    //         .required("Password isRequired")
+    //         .matches(/(?=.*[A-Z])/, "Password isRequired")
+    //         .matches(/(?=.*[0-9])/, "Password must contain at least one number")
+    //         .matches(
+    //             /(?=.*[!@#$%^&*])/,
+    //             "Password must contain one of the special characters !@#$%^&*"
+    //         )
+    //         .matches(
+    //             /(?=.{8,})/,
+    //             "Password must be at least 8 characters long"
+    //         ),
+    //     email: yup
+    //         .string()
+    //         .required("Email isRequired")
+    //         .email("Email entered incorrectly")
+    // });
 
-    // const validatorConfig = {
-    //     email: {
-    //         isRequired: { message: "Email isRequired" },
-    //         isEmail: { message: "Email entered incorrectly" }
-    //     },
-    //     password: {
-    //         isRequired: { message: "Password isRequired" },
-    //         isCapitalSymbol: {
-    //             message: "Password must contain at least one uppercase letter"
-    //         },
-    //         isContainDigit: {
-    //             message: "Password must contain at least one number"
-    //         },
-    //         min: {
-    //             message: "Password must be at least 8 characters long",
-    //             value: 8
-    //         }
-    //     }
-    // };
+    const validatorConfig = {
+        email: {
+            isRequired: { message: "Email isRequired" },
+            isEmail: { message: "Email entered incorrectly" }
+        },
+        password: {
+            isRequired: { message: "Password isRequired" },
+            isCapitalSymbol: {
+                message: "Password must contain at least one uppercase letter"
+            },
+            isContainDigit: {
+                message: "Password must contain at least one number"
+            },
+            min: {
+                message: "Password must be at least 8 characters long",
+                value: 8
+            }
+        }
+    };
 
     useEffect(() => {
         console.log(errors);
@@ -65,12 +64,8 @@ const LoginForm = () => {
     }, [data]);
 
     const validate = () => {
-        // const errors = validator(data, validatorConfig);
-        validateScheme
-            .validate(data)
-            .then(() => setErrors({}))
-            .catch((error) => setErrors({ [error.path]: error.message }));
-        // setErrors(errors);
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
         return Object.keys(errors).length === 0;
     };
     const isValid = Object.keys(errors).length === 0;
